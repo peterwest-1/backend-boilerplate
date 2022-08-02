335b700062db0002c5bc131d241d2d37f60dd63d
Pete West <10847430+peterwest-1@users.noreply.github.com>
2022-08-02 10:36:46 +0200

    Added registeration validation and confirmation logic

---
+++ src/utilities/sendMail.ts
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer";
+
+export async function sendEmail(email: string, url: string) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "mipp5fj52yrrai6y@ethereal.email",
+      pass: "W27uppXc7JtE7qhxJq",
+    },
+  });
+
+  const mailOptions = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: email, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: `<a href="${url}">${url}</a>`, // html body
+  };
+
+  const info = await transporter.sendMail(mailOptions);
+
+  console.log("Message sent: %s", info.messageId);
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
--- src/validators/register.ts
+++ src/validators/register.ts
@@ -1,21 +1,22 @@
-import { DUPLICATE_EMAIL, INVALID_EMAIL, PASSWORD_LENGTH } from "../shared/Errors";
+import { AuthenticationInput } from "../shared/AuthenticationInput";
+import { DUPLICATE_EMAIL, INVALID_EMAIL } from "../shared/EmailErrors";
+import { PASSWORD_LENGTH } from "../shared/PasswordErrors";
 import { User } from "../entity/User";
-import { AuthenticationInput } from "../resolvers/user";
 
 export const validateRegister = async (options: AuthenticationInput) => {
   await User.find({ where: { email: options.email } }).then((user) => {
     if (user) {
-      return DUPLICATE_EMAIL;
+      return [DUPLICATE_EMAIL];
     }
     return true;
   });
 
   if (!options.email.includes("@")) {
-    return INVALID_EMAIL;
+    return [INVALID_EMAIL];
   }
 
   if (options.password.length <= 2) {
-    return PASSWORD_LENGTH;
+    return [PASSWORD_LENGTH];
   }
 
   return null;
