b3c51583ff1273d876142ecf9fbc2b2a08b65c3c
Pete West <10847430+peterwest-1@users.noreply.github.com>
2022-08-02 10:36:18 +0200

    Abstracted data sources

--- src/test-data-source.ts
+++ src/test-data-source.ts
@@ -1,14 +1,12 @@
-import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { __prod__ } from "./constants";
 import { Contractor } from "./entity/Contractor";
 import { Review } from "./entity/Review";
 import { User } from "./entity/User";
-require("dotenv").config();
 
-export const AppDataSource = new DataSource({
+export const TestDataSource = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL,
+  url: process.env.TESTING_DATABASE_URL,
   synchronize: !__prod__,
   logging: !__prod__,
   entities: [User, Contractor, Review],
