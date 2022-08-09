e16777f026ceb5acd90e3b5cdc51aefe19c4497d
Pete West <10847430+peterwest-1@users.noreply.github.com>
2022-08-09 17:53:11 +0200

    Abstracted some resolver logic

--- src/utilities/createSchema.ts
+++ src/utilities/createSchema.ts
@@ -1,8 +1,10 @@
 import { UserResolver } from "../resolvers/user";
 import { buildSchema } from "type-graphql";
+import { ChangePasswordResolver } from "../resolvers/ChangePasswordResolver";
+import { ForgotPasswordResolver } from "../resolvers/ForgotPasswordResolver";
 
 export const createSchema = () =>
   buildSchema({
-    resolvers: [UserResolver],
+    resolvers: [UserResolver, ChangePasswordResolver, ForgotPasswordResolver],
     validate: false,
   });
