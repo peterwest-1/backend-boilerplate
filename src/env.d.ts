1270d0d2120f2e37d53d6f86ab42afba6f6acfe9
Pete West <10847430+peterwest-1@users.noreply.github.com>
2022-09-09 09:48:38 +0200

    Added exmaple .env and .env type gen

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DATABASE_URL: string;
+      TESTING_DATABASE_URL: string;
       REDIS_URL: string;
       PORT: string;
       CORS_ORIGIN: string;
